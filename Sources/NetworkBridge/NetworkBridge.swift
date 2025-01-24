91b20d673bed4c0e5c61fc71629fb7837150c817
RafatMeraz <razhossain201@gmail.com>
2025-01-24 21:29:01 +0600

    Update NetworkBridge class access modifier

--- Sources/NetworkBridge/NetworkBridge.swift
+++ Sources/NetworkBridge/NetworkBridge.swift
@@ -1,28 +1,28 @@
 import Foundation
 
-class NetworkBridge {
+public class NetworkBridge {
     private let commonHeaders: [String: String]
     private let errorMapper: ErrorMapper
     
-    public init(commonHeaders: [String: String], errorMapper: ErrorMapper = DefaultErrorMapper()) {
+    init(commonHeaders: [String: String], errorMapper: ErrorMapper = DefaultErrorMapper()) {
         self.commonHeaders = commonHeaders
         self.errorMapper = errorMapper
     }
     
     // Expose specific request classes
-    public func getRequest() -> GetRequest {
+    func getRequest() -> GetRequest {
         return GetRequest(commonHeaders: commonHeaders, errorMapper: errorMapper)
     }
     
-    public func postRequest() -> PostRequest {
+    func postRequest() -> PostRequest {
         return PostRequest(commonHeaders: commonHeaders, errorMapper: errorMapper)
     }
     
-    public func putRequest() -> PutRequest {
+    func putRequest() -> PutRequest {
         return PutRequest(commonHeaders: commonHeaders, errorMapper: errorMapper)
     }
     
-    public func deleteRequest() -> DeleteRequest {
+    func deleteRequest() -> DeleteRequest {
         return DeleteRequest(commonHeaders: commonHeaders, errorMapper: errorMapper)
     }
 }
